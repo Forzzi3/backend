d290f8157fc36944618ebd36bf43f9bb9d88cf70
08032004Aa! <egorrudakov789@gmail.com>
2025-09-21 12:00:36 +0300

    backend 1.0

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/save', (req, res) => {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).json({ error: 'Нет сообщения' });
+  }
+
+  fs.appendFile('data.txt', message + '\n', (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Трабл в сохранении файла' });
+    }
+    res.json({ status: 'ok', saved: message });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
